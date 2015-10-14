7ff7cd0dfaa1a305a32d49bbd421269acbb77c2d
Artem <artem200@yandex.ru>
2015-10-14 16:17:23 +0300

    server time init

--- Backend/jata.py
+++ Backend/jata.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "JATA.RU"
+    return "JATA.RU <br>Time now: " + str(datetime.datetime.now().time())
 
 if __name__ == '__main__':
     app.run(
