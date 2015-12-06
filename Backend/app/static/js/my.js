731a06032ae3f3c4371370b2ba81f9f19b4c9bcd
Artem <artem200@yandex.ru>
2015-12-06 23:13:46 +0300

    smart map search

--- Backend/app/static/js/my.js
+++ Backend/app/static/js/my.js
@@ -131,6 +131,7 @@
 				ymaps.ready(init);
 
 				function init () {
+					var myPlacemark;
 					myMap = new ymaps.Map('map', {
 						center: [59.95, 30.2],
 						zoom: 10
@@ -138,9 +139,54 @@
 						searchControlProvider: 'yandex#search'
 					});
 
-					document.getElementById('destroyButton').onclick = function () {
-						myMap.destroy();
-					};
+					// document.getElementById('destroyButton').onclick = function () {
+					// 	myMap.destroy();
+					// };
+
+					// Слушаем клик на карте
+				    myMap.events.add('click', function (e) {
+				        var coords = e.get('coords');
+
+				        // Если метка уже создана – просто передвигаем ее
+				        if (myPlacemark) {
+				            myPlacemark.geometry.setCoordinates(coords);
+				        }
+				        // Если нет – создаем.
+				        else {
+				            myPlacemark = createPlacemark(coords);
+				            myMap.geoObjects.add(myPlacemark);
+				            // Слушаем событие окончания перетаскивания на метке.
+				            myPlacemark.events.add('dragend', function () {
+				                getAddress(myPlacemark.geometry.getCoordinates());
+				            });
+				        }
+				        getAddress(coords);
+				    });
+
+				    // Создание метки
+				    function createPlacemark(coords) {
+				        return new ymaps.Placemark(coords, {
+				            iconContent: 'поиск...'
+				        }, {
+				            preset: 'islands#lightBlueStretchyIcon',
+				            draggable: true
+				        });
+				    }
+
+				    // Определяем адрес по координатам (обратное геокодирование)
+				    function getAddress(coords) {
+				        myPlacemark.properties.set('iconContent', 'поиск...');
+				        ymaps.geocode(coords).then(function (res) {
+				            var firstGeoObject = res.geoObjects.get(0);
+
+				            myPlacemark.properties
+				                .set({
+				                    iconContent: firstGeoObject.properties.get('name'),
+				                    balloonContent: firstGeoObject.properties.get('text')
+				                });
+				        });
+				    }
+
 
 				}
 				
